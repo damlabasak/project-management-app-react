42adf2bfa98c49270a91238a59f05ea134c9d48c
Damla Başak Turgut <kocakdamlabasak@gmail.com>
2024-04-22 06:11:00 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-// App bileşenini render etmek için createRoot kullan
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
